a40c012775f91772dbfaf2fbaf8bf265f58926e3
Karthik Kunjithapatham <karthikk@Karthiks-MacBook-Pro.local>
2019-02-08 11:46:11 +1100

    Working stomp websocket project

---
+++ initial/src/main/java/messageman/Message.java
@@ -0,0 +1,32 @@
+package messageman;
+
+public class Message {
+	
+	private String name;
+	
+	public Message() {
+		
+	}
+	
+	
+
+	/**
+	 * @param name
+	 */
+	public Message(String name) {
+		super();
+		this.name = name;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+}
